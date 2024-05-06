ff2783bf9e7ad6a8dfdf4c5f413bfe6105753b76
Tomáš Kropáček <kropacek@rtsoft.cz>
2024-05-06 11:47:51 +0200

    Zapnutí Tracy

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -15,7 +15,8 @@ class Bootstrap
 		$appDir = dirname(__DIR__);
 
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy($appDir . '/log');
+        $configurator->setDebugMode(true);
+        $configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTempDirectory($appDir . '/temp');
 
